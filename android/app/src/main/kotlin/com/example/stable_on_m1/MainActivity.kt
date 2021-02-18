ee908e8aa0c3f41e1a63bc7f7bd90ebf3d1f56e6
Kilo Loco <kyle@kiloloco.com>
2021-02-17 16:45:10 -0800

    working android m1

---
+++ android/app/src/main/kotlin/com/example/stable_on_m1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stable_on_m1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_datastore/amplify_datastore.dart';
+import 'package:amplify_datastore/types/DataStoreHubEvents/DataStoreHubEvent.dart';
+import 'package:amplify_flutter/amplify.dart';
+import 'package:flutter/material.dart';
+
+import 'amplifyconfiguration.dart';
+import 'models/ModelProvider.dart';
+import 'todos_view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _amplifyConfigured = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  void _configureAmplify() async {
+    try {
+      final dataStorePlugin =
+          AmplifyDataStore(modelProvider: ModelProvider.instance);
+      await Amplify.addPlugin(dataStorePlugin);
+      await Amplify.addPlugin(AmplifyAPI());
+
+      await Amplify.configure(amplifyconfig);
+      print('Amplify configured');
+      // await Amplify.DataStore.clear();
+      // print('cleared datastore');
+      _observeHub();
+      setState(() => _amplifyConfigured = true);
+    } on AmplifyAlreadyConfiguredException catch (amplifyException) {
+      print(amplifyException.message);
+      print('Amplify not configured for restart');
+    } catch (e) {
+      print('error occured: ${e.runtimeType}');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MaterialApp(
+          home: _amplifyConfigured
+              ? TodosView()
+              : Center(
+                  child: CircularProgressIndicator(),
+                )),
+    );
+  }
+
+  void _observeHub() {
+    final sub = Amplify.Hub.listen([HubChannel.DataStore], (hubEvent) {
+      final dshubevent = hubEvent as DataStoreHubEvent;
+      print('hub event: ${dshubevent.eventName}');
+    });
+    print(sub);
+  }
+}
